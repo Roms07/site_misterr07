f3513f035261523a933d2ed4bb53f8ed33f25922
Roms07 <ribetromain@yahoo.fr>
2019-03-09 17:00:36 +0100

    Start to code the display of char's infos

--- js/algo_chars.js
+++ js/algo_chars.js
@@ -1,21 +1,28 @@
-
 $(document).ready(function () {
-	let chars = [];
-	// Création de la "liste"
-	$(function () {
-		// Grab the template script
-		let theTemplateScript = $("#chars-template").html();
+    const values = ['link', 'sprite', 'description', 'image'];
+    // Création de la "liste"
+    $(function () {
+        // Grab the template script
+        let theTemplateScript = $("#chars-template").html();
 
-		// Compile the template
-		let theTemplate = Handlebars.compile(theTemplateScript);
+        // Compile the template
+        let theTemplate = Handlebars.compile(theTemplateScript);
 
-		// Define our data object
-		$.getJSON("js/chars.json", {data: "json"}, function (response) {
-			// Pass our data to the template
-			let theCompiledHtml = theTemplate(response);
-			// Add the compiled html to the page
-			$('#chars').prepend(theCompiledHtml);
-		});
-	});
+        // Define our data object
+        $.getJSON("js/chars.json", {data: "json"}, function (response) {
+            // Pass our data to the template
+            let theCompiledHtml = theTemplate(response);
+            // Add the compiled html to the page
+            $('#chars').prepend(theCompiledHtml);
+        });
+    });
 
+    $('#chars').on('click', '.perso', function () {
+        let char = [];
+        for (let i = 0; i < values.length; i++) {
+            let goodInput = 'input[name=' +values[i] +']';
+            char[values[i]] = $(this).find(goodInput).get(0).value;
+        }
+       console.log(char);
+    })
 });
