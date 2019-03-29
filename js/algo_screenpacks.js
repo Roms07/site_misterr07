0b07261674e4b2f7f6755c4b1b0020685168500e
roms07 <ribetromain@yahoo.fr>
2019-03-29 22:26:51 +0100

    Add screenpacks page

--- js/algo_screenpacks.js
+++ js/algo_screenpacks.js
@@ -1,20 +1,19 @@
+$(document).ready(function () {
 
-jQuery(document).ready(function ($) {
-	$.getJSON('js/screenpacks.json', function(screenpacks) {
-			$('#screenpacks').html('');
-			for (var i=0; i<screenpacks.length; i++){
-				$('#screenpacks').append(
-					'<div class="screenpack container-fluid col-lg-6">'
-						+'<div class=row>'
-							+'<div class="col-lg-6">'						
-								+'<img class="img_screenpack" src=' +screenpacks[i].image +' alt='+screenpacks[i].id +'>'
-							+'</div>'
-							+'<div class="col-lg-6">'						
-								+'<p>' +screenpacks[i].description +'</p>'
-							+'</div>'
-						+'</div>'
-						+'<a href="'+screenpacks[i].link +'"> Link</a>'
-					+'</div>'
-			)};
+	// Création de la "liste"
+	$(function () {
+		// Grab the template script
+		let theTemplateScript = $("#screenpacks-template").html();
+
+		// Compile the template
+		let theTemplate = Handlebars.compile(theTemplateScript);
+
+		// Define our data object
+		$.getJSON("js/screenpacks.json", {data: "json"}, function (response) {
+			// Pass our data to the template
+			let theCompiledHtml = theTemplate(response);
+			// Add the compiled html to the page
+			$('#screenpacks').prepend(theCompiledHtml);
 		});
+	});
 });
