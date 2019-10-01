d2b06b638a55bf313f2e6d9da3abbf33b7bc1e93
Roms07 <ribetromain@yahoo.fr>
2019-10-01 16:53:39 +0200

    Make menu responsive

---
+++ js/responsive.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    function resizeNavBar(height) {
+        let top = $('#img').height();
+        let navBar = $('#nav-bar');
+        navBar.css('top', top);
+        let newHeight = height - top - $('.toggle-nav').height() - 19;
+        navBar.css('height', newHeight);
+        $('.navs-link').css('height', newHeight / 5);
+    }
+
+    $('.toggle-nav-link').click( function (event) {
+        event.preventDefault();
+        $('.toggle-nav').toggleClass('visible');
+        $('#nav-bar').toggleClass('visible');
+        $('.content').toggleClass('visible');
+    });
+    let height = window.innerHeight;
+    let width = window.innerWidth;
+
+    if (width < 767 ) {
+        resizeNavBar(height);
+    }
+
+    $(window).resize( function () {
+        let height = window.innerHeight;
+        let width = window.innerWidth;
+        if (width < 767 ) {
+            resizeNavBar(height);
+        } else {
+            $('#nav-bar').css('height', '');
+            $('.navs-link').css('height', '');
+        }
+    })
+});
