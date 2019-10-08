a78c62001af23940de0753070800aae33836d932
Roms07 <ribetromain@yahoo.fr>
2019-10-08 18:59:06 +0200

    Fix display of chars page

--- js/algo_chars.js
+++ js/algo_chars.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    const values = ['link', 'sprite', 'description', 'image'];
+    const values = ['name','link', 'sprite', 'description', 'image'];
     // Création de la "liste"
     $(function () {
         // Grab the template script
@@ -18,31 +18,44 @@ $(document).ready(function () {
         });
     });
 
-
-
     $('#chars').on('click', '.perso', function () {
         let char = [];
+        let width = window.innerWidth;
+
         for (let i = 0; i < values.length; i++) {
             let goodInput = 'input[name=' +values[i] +']';
             char[values[i]] = $(this).find(goodInput).get(0).value;
         }
 
+        if (width < 768) {
+            $('#chars').removeClass('visible');
+            $('#view').addClass('visible');
+            $('.close-view').addClass('visible');
+        }
+
        $('#view').html(
        '<div class="choice container-fluid">'
            +'<div class="row mt-3">'
-               +'<div class="col-lg-6">'
+               +'<div class="col-6 p-0">'
                     +'<img class="big-portrait" alt="" src=' +char.image + '>'
                +'</div>'
-               +'<div class="col-lg-6 text-center">'
-                   +'<h3 class="name-char mt-4">Goku</h3>'
-                   +'<img class="sprite mt-3" alt="Goku" src=' +char.sprite +'>'
-                   +'<a href='+char.link +'><button class="link mt-5">Telecharger</button></a>'
+               +'<div class="col-6 text-center">'
+                   +'<h3 class="name-char">'+char.name+'</h3>'
+                   +'<img class="sprite" alt="Goku" src=' +char.sprite +'>'
+                   +'<a target="_blank" class="link" href='+char.link +'>Telecharger</a>'
                +'</div>'
            +'</div>'
-           +'<div class="row mt-4">'
+           +'<div class="row">'
                 +'<p class="description">' +char["description"] +'</p>'
            +'</div>'
         +'</div>'
        );
-    })
+    });
+
+    $('.close-view').on('click', function (event) {
+        event.preventDefault();
+        $('#chars').addClass('visible');
+        $('#view').removeClass('visible');
+        $('.close-view').removeClass('visible');
+    });
 });
