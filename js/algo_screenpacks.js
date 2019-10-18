dbf6eba724397568d73ca4dfc6389fc720bd9dc3
Roms07 <ribetromain@yahoo.fr>
2019-10-18 17:31:55 +0200

    Make new carousel for screenpacks

--- js/algo_screenpacks.js
+++ js/algo_screenpacks.js
@@ -18,6 +18,22 @@ $(document).ready(function () {
 
 			// We calculate the height of the video to keep the ratio 4/3 at the creation of the page
 			resizeVideo($('.video-screenpack'));
+
+			// Carousel with slick
+			let mySwiper = new Swiper ('.swiper-container', {
+				// Optional parameters
+				speed: 500,
+				effect: 'fade',
+				grabCursor: true,
+				loop: true,
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+				},
+				pagination: {
+					el: '.swiper-pagination',
+				},
+			})
 		});
 	});
 
